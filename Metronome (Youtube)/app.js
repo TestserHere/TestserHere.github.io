e97fed2cdbbfb8405100ac444547015ab43085a2
TestserHere <charlesmeng2012@gmail.com>
2024-09-22 01:29:28 +0000

    save

--- Metronome (Youtube)/app.js
+++ Metronome (Youtube)/app.js
@@ -7,17 +7,17 @@ const increaseTempoBtn = document.querySelector('.increase-tempo');
 const tempoSlider = document.querySelector('.slider');
 const startStopBtn = document.querySelector('.start-stop');
 const subtractBeats = document.querySelector('.subtract-beats');
-const addBeats = document.querySelector('.add-beats')
-const measureCount = document.querySelector('.measure-count')
+const addBeats = document.querySelector('.add-beats');
+const measureCount = document.querySelector('.measure-count');
 
-const click1 = new Audio('click1.mp3')
-const click2 = new Audio('click2.mp3')
+const click1 = new Audio('click1.mp3');
+const click2 = new Audio('click2.mp3');
 
 let bpm = 140;
 let beatsPerMeasure = 4;
 let count = 0;
 let isRunning = false;
-let tempoTextString = 'Meduim';
+let tempoTextString = 'Medium';
 
 decreaseTempoBtn.addEventListener('click', () => {
     if (bpm <= 20) { return };
@@ -31,12 +31,11 @@ increaseTempoBtn.addEventListener('click', () => {
     validateTempo();
     updateMetronome();
 });
-
 tempoSlider.addEventListener('input', () => {
-    bpm = tempoSlider.ariaValueMax;
+    bpm = tempoSlider.value;
     validateTempo();
     updateMetronome();
-})
+});
 
 subtractBeats.addEventListener('click', () => {
     if (beatsPerMeasure <= 2) { return };
@@ -52,35 +51,33 @@ addBeats.addEventListener('click', () => {
 });
 
 startStopBtn.addEventListener('click', () => {
-   count =0;
-   if (!isRunning) {
+    count = 0;
+    if (!isRunning) {
         metronome.start();
         isRunning = true;
-        startStopBtn.textContent = 'STOP'
-   } else {
+        startStopBtn.textContent = 'STOP';
+    } else {
         metronome.stop();
         isRunning = false;
         startStopBtn.textContent = 'START';
-   }
+    }
 });
 
 function updateMetronome() {
     tempoDisplay.textContent = bpm;
     tempoSlider.value = bpm;
     metronome.timeInterval = 60000 / bpm;
-    if(bpm <= 40) {tempoTextString = "Super Slow"}
-    if(bpm > 40 && bpm < 80) {tempoTextString = "Slow"}
-    if(bpm > 80 && bpm < 120) {tempoTextString = "Getting There"}
-    if(bpm > 120 && bpm < 180) {tempoTextString = "Nice and Steady"}
-    if(bpm > 180 && bpm < 220) {tempoTextString = "Rock n' Roll"}
-    if(bpm > 220 && bpm < 240) {tempoTextString = "Funky Stuff"}
-    if(bpm > 240 && bpm < 260) {tempoTextString = "Relax Dude"}
-    if(bpm > 260 && bpm <= 280) {tempoTextString = "Eddie Van Halen"}
-    
+    if (bpm <= 40) { tempoTextString = "Super Slow" };
+    if (bpm > 40 && bpm < 80) { tempoTextString = "Slow" };
+    if (bpm > 80 && bpm < 120) { tempoTextString = "Getting there" };
+    if (bpm > 120 && bpm < 180) { tempoTextString = "Nice and Steady" };
+    if (bpm > 180 && bpm < 220) { tempoTextString = "Rock n' Roll" };
+    if (bpm > 220 && bpm < 240) { tempoTextString = "Funky Stuff" };
+    if (bpm > 240 && bpm < 260) { tempoTextString = "Relax Dude" };
+    if (bpm > 260 && bpm <= 280) { tempoTextString = "Eddie Van Halen" };
+
     tempoText.textContent = tempoTextString;
-
 }
-
 function validateTempo() {
     if (bpm <= 20) { return };
     if (bpm >= 280) { return };
@@ -96,9 +93,9 @@ function playClick() {
         click1.currentTime = 0;
     } else {
         click2.play();
-        click2.currentTime =0;
+        click2.currentTime = 0;
     }
     count++;
 }
 
-const metronome = newTimer(playClick, 60000 / bpm, { immediate: true })
+const metronome = new Timer(playClick, 60000 / bpm, { immediate: true });
