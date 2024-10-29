e7b51ac6c48a44d7a50d02c78c423c9b31c66d5f
TestserHere <charlesmeng2012@gmail.com>
2024-10-29 03:45:38 +0000

    Keyboard

--- Keyboard_Test/code.js
+++ Keyboard_Test/code.js
@@ -1,11 +1,44 @@
 function keyUpdate() {
-    document.addEventListener("keyUp", (e) => {
-        if (e.code == "ArrowUp") {
-            alert("Test");
-            document.getElementById("keyboard_1").style.color = "Black";
-        } else {
-            keyUpdate();
+    document.addEventListener("keydown", (e) => {
+        console.log(e.key, e.code);
+
+        let id = "keyboard_" + e.key.toLowerCase()
+
+        if (e.key == "Backspace") {
+            id = "keyboard_Backspace";
+        } else if (e.code == "CapsLock") {
+            id = "keyboard_Caps";
+        } else if (e.code == "LeftShift") {
+            id = "keyboard_Shift1";
+        } else if (e.code == "RightShift") {
+            id = "keyboard_Shift2";
         }
-        
+
+
+
+        document.getElementById(id).style.color = "black";
     })
+
+    document.addEventListener("keyup", (e) => {
+        console.log(e.key, e.code);
+
+        let id = "keyboard_" + e.key.toLowerCase()
+
+        if (e.key == "Backspace") {
+            id = "keyboard_Backspace"
+        } else if (e.code == "CapsLock") {
+            id = "keyboard_Caps";
+        }
+
+
+        document.getElementById(id).style.color = "white";
+    })
+
+
+
 }
+
+
+
+
+window.onload = keyUpdate;
