e70fa0929c10c992e139852bae47c8dbc98eb1b1
TestserHere <charlesmeng2012@gmail.com>
2024-11-16 09:59:09 +0000

    saved

--- home_things/scripts.js
+++ home_things/scripts.js
@@ -101,23 +101,3 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('exampleModal').addEventListener('hidden.bs.modal', removeBackdrops);
     document.getElementById('imagePreviewModal').addEventListener('hidden.bs.modal', removeBackdrops);
 });
-
-
-// // Disable right-click
-// document.addEventListener('contextmenu', (e) => e.preventDefault());
-
-// function ctrlShiftKey(e, keyCode) {
-//   return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
-// }
-
-// document.onkeydown = (e) => {
-//   // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
-//   if (
-//     event.keyCode === 123 ||
-//     ctrlShiftKey(e, 'I') ||
-//     ctrlShiftKey(e, 'J') ||
-//     ctrlShiftKey(e, 'C') ||
-//     (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
-//   )
-//     return false;
-// };
