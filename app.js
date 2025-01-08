3f4954c56f7ac1437940d5a60f2036677f3d904d
TestserHere <charlesmeng2012@gmail.com>
2025-01-08 07:14:20 +0000

    public

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/test', (req, res) => {
+  res.send('hello, world');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
