26d7d830be21680dc554723a6f2ef2251ede0680
TestserHere <charlesmeng2012@gmail.com>
2025-01-08 07:38:43 +0000

    add apis

--- api/index.js
+++ api/index.js
@@ -11,6 +11,29 @@ app.get('/test', (req, res) => {
   res.send('hello, world');
 });
 
+// API to set an item
+app.post('/setItem', (req, res) => {
+  const { key, value } = req.body;
+
+  if (!key || value === undefined) {
+    return res.status(400).json({ error: 'Key and value are required.' });
+  }
+
+  storage[key] = value;
+  res.json({ message: `Item set: ${key}` });
+});
+
+// API to get an item
+app.get('/getItem/:key', (req, res) => {
+  const key = req.params.key;
+
+  if (key in storage) {
+    res.json({ key, value: storage[key] });
+  } else {
+    res.status(404).json({ error: `Item with key '${key}' not found.` });
+  }
+});
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
