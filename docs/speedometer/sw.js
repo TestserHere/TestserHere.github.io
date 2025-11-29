d6771e22b6e1d2d86ef0b60d4834552efe165661
TestserHere <charlesmeng2012@gmail.com>
2025-11-29 13:21:02 +1100

    update v2

--- docs/speedometer/sw.js
+++ docs/speedometer/sw.js
@@ -1,8 +1,22 @@
 const CACHE = 'speedometer-v1';
-const FILES = ['speedometer.html', 'speedometer.png'];
+const FILES = ['index.html', 'speedometer.png', 'sw.js'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return Promise.all(
+        FILES.map(file => {
+          return fetch(file).then(response => {
+            if (response.ok) {
+              return cache.put(file, response);
+            }
+          }).catch(err => {
+            console.warn(`Failed to cache ${file}:`, err);
+          });
+        })
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', e => {
